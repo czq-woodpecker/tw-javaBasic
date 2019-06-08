68c2f542dbf8e22f12540fbf41e1c23d8e899bcf
woodpecker <czq.woodpecker@qq.com>
2019-06-08 23:36:19 +0800

    finish section3 PracticeC

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,18 @@
 package cn.school.thoughtworks.section3;
-
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> countMap = new HashMap<>();
+        int count;
+        for (String str : collectionA) {
+            count = countMap.get(str) == null ? 0 : countMap.get(str);
+            countMap.put(str, ++count);
+        }
+        PracticeB practiceB = new PracticeB();
+        Map<String, Integer> updatedCollection = practiceB.createUpdatedCollection(countMap, object);
+        return updatedCollection;
     }
 }
