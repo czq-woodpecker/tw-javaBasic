9f09e42484315d75c4d88ebbc220478e439610e1
woodpecker <czq.woodpecker@qq.com>
2019-06-08 20:25:58 +0800

    finish section1 PracticeC

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -2,10 +2,19 @@ package cn.school.thoughtworks.section1;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
-    List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+    List<String> collectSameElements(List<String> collection1, Map<String, List<String>> collection2) {
+        List<String> c2 = collection2
+                .values()
+                .stream()
+                .flatMap(e -> e.stream())
+                .collect(Collectors.toList());
+        List<String> resultList = collection1
+                .stream()
+                .filter(item -> c2.contains(item))
+                .collect(Collectors.toList());
+        return resultList;
     }
 }
