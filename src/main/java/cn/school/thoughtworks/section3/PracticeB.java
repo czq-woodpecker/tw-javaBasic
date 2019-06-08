df4460ffcec62ebd14c22d4efb52254b00940796
woodpecker <czq.woodpecker@qq.com>
2019-06-08 23:23:08 +0800

    finish section3 PracticeB

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -2,11 +2,17 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        List<String> collectionB = object.values().stream().flatMap(item -> item.stream()).collect(Collectors.toList());
+        int count;
+        for (String str : collectionB) {
+            if(collectionA.containsKey(str) && (count = collectionA.get(str)) >= 3) {
+                collectionA.put(str, count - count / 3);
+            }
+        }
+        return collectionA;
     }
 }
