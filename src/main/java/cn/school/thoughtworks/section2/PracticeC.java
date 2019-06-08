5a048f447b8399f49c0b5c8d3b5f1bc4c8008e6c
woodpecker <czq.woodpecker@qq.com>
2019-06-08 23:08:21 +0800

    finish section2 PracticeC

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,33 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
 
-        return null;
+        Map<String, Integer> countMap = new HashMap<>();
+        int count = 0;
+        for (String str : collection1) {
+            if (str.contains("-")) {
+                String[] split = str.split("-");
+                str = split[0];
+                count = Integer.parseInt(split[1]);
+            } else if (str.contains(":")) {
+                String[] split = str.split(":");
+                str = split[0];
+                count = Integer.parseInt(split[1]);
+            } else if (str.contains("[")) {
+                String[] split = str.split("\\[");
+                str = split[0];
+                count = Integer.parseInt(split[1].substring(0, split[1].length() - 1));
+            } else {
+                count = 1;
+            }
+            count += countMap.get(str) == null ? 0 : countMap.get(str);
+            countMap.put(str, count);
+        }
+        return countMap;
     }
 }
